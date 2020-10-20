510d0640e146ffb91d5ce74697e24391eb82fe86
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-10-20 21:26:17 +0000

    working css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 const port = process.env.port || 3000;
 var app = express();
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.get('/',function(req,res){
     res.render('index');
 
