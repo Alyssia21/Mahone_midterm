497b32822315b28d314ab78aa7730686320075fc
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-10-21 15:35:12 +0000

    working background

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.get('/',function(req,res){
     res.render('index');
-
+    
 });
+
+app.get('/random',function(req,res){
+    res.render('random');
+    
+});
+
 app.listen(port,function(){
     console.log('listening on'+port)
 });
+
