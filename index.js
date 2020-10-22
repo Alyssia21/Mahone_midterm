1e73b484066e4b3a7457b29a5695718a31b03c5b
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-10-22 03:45:41 +0000

    working fetch

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+const fetch = require('node-fetch');
 var express = require('express');
 const port = process.env.port || 3000;
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+
 app.get('/',function(req,res){
-    res.render('index');
+    fetch('http://xkcd.com/info.0.json')
+    .then(response =>  response.json())
+    .then(data => {
+    //console.log('SUCCESS!' + data)
+    res.render('index',{data:data});
+    });
     
-});
+})
 
 app.get('/random',function(req,res){
-    res.render('random');
+    fetch('http://xkcd.com/614/info.0.json' )
+    .then(response =>  response.json())
+    .then(data => {
+   //console.log(data)
+    res.render('random',{data:data})
+    });  
+})
+
     
-});
-
 app.listen(port,function(){
-    console.log('listening on'+port)
+    console.log('listening on '+port)
 });
-
