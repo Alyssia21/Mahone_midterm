46f9438fa1158ae39f754c04741b0a03826dee8d
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-10-23 11:32:12 +0000

    heroku config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 var express = require('express');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
